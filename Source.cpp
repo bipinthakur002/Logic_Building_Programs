693a7d0cab6d6db384f935b241540e9ca36827b6
Bipin Thakur <30946579+bipinthakur002@users.noreply.github.com>
2018-09-08 03:50:05 +0530

    Add files via upload
    Program for Decimal to binary in c

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,33 @@
-#include"Header.h"
+#include<stdio.h>
+#include<stdlib.h>
+void DisplayDecToBin(int n)
+{
+	int *arr;
+	int i = 0;
+	int j = 0;
+	arr = (int *)malloc(sizeof(int));
+	while (n > 0)
+	{
+
+
+		arr[i] = n % 2;
+		n = n / 2;
+		i++;
+	}
+
+	for (j = i-1;j >= 0;j--)
+	{
+		printf("%d", arr[j]);
+	}
+
+
+}
+
 int main()
 {
 	int num = 0;
-	printf("enter  number:\n");
+	printf("enter number:\n");
 	scanf_s("%d", &num);
-	Evenfactor(num);
+	DisplayDecToBin(num);
 	return 0;
-
 }
