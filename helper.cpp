a7d9868a661e25a840c6cd92bd38f1dcdfc0b073
Bipin Thakur <30946579+bipinthakur002@users.noreply.github.com>
2018-09-08 02:28:58 +0530

    Update helper.cpp

--- helper.cpp
+++ helper.cpp
@@ -2,8 +2,14 @@
 void Evenfactor(int ino)
 {
 	int result = 0;
+	if(ino<0)
+	{
+	ino=-ino;	
+	}
+	
 	int i = 0;
-	for (i = 1;i <= ino / 2;i++)
+
+	for (i = 1 ; i <= ino / 2 ; i++)
 	{
 		if (i % 2 == 0)
 		{
